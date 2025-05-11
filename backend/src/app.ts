dc588b70d49869dc245b09a06fd04728eff81fb2
Anushka Yadav <anushkay0092@gmail.com>
2025-05-11 23:27:18 +0530

    added vercel config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,10 +13,6 @@ app.use(express.json());
 app.use("/api", routes);
 
 
-// Your routes go here
-// app.use("/api/users", userRoutes);
-// app.use("/api/boards", boardRoutes);
-
 app.use(errorHandler as unknown as express.ErrorRequestHandler);
 
 export default app;

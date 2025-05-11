ee2ff78ae368a89e0fdb0022157851a8ae13cef1
Anushka Yadav <anushkay0092@gmail.com>
2025-05-11 23:58:32 +0530

    added vercel config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,11 +7,15 @@ import routes from "./routes";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+    "http://localhost:3000",
+    "https://kanban-board-iota-sandy.vercel.app/"
+    ]
+}));
 app.use(express.json());
 
 app.use("/api", routes);
-
 
 app.use(errorHandler as unknown as express.ErrorRequestHandler);
 
